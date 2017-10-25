43a4b3ae70e4615b622aff2a44a35d1a15fcd765
palmpc <palmpc@LG-15U370-LR20K>
2017-10-25 18:01:57 +0900

    Practice Java Programming

---
+++ src/Application.java
@@ -0,0 +1,27 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int[][] numbers = {
+				{2,3,20},
+				{1,3,10},
+				{100, 200, 400}
+		};
+		
+		for ( int x = 0; x < numbers.length; x++) {
+			
+			for( int i = 0; i < numbers[x].length; i++) {
+				System.out.println(numbers[x][i]);
+			}
+		}
+	}	
+	
+
+	
+	
+	
+}
+		
